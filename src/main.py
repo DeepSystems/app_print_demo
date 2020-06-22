148241b69b2b78151e58c3eb088a692f84a07a97
Maxim Kolomeychenko <max@supervise.ly>
2020-06-22 17:36:48 +0300

    demo error is fixed

--- src/main.py
+++ src/main.py
@@ -23,8 +23,8 @@ def main():
 
     # Example:
     #SOURCE_DIR = os.path.dirname(os.path.realpath(__file__))
-    my_app.run(template_path=os.path.join(SOURCE_DIR, 'gui.html'), data=data, state=state)
-    #my_app.run(data=data, state=state)
+    #my_app.run(template_path=os.path.join(SOURCE_DIR, 'gui.html'), data=data, state=state)
+    my_app.run(data=data, state=state)
 
 
 if __name__ == "__main__":
